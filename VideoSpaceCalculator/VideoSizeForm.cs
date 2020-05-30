9efca05bcf03ab5b3cf157935502325f3f7f6d88
umitunal1 <ce.umitunal@gmail.com>
2020-05-30 13:51:08 +0300

    edited

--- VideoSpaceCalculator/VideoSizeForm.cs
+++ VideoSpaceCalculator/VideoSizeForm.cs
@@ -363,7 +363,7 @@ namespace VideoSpaceCalculator
 
             totalAudioSize = totalSecond() * calculateAudioSize(audioChannel, hertz, bit);
             totalMaterialSize = totalVideoSize + totalAudioSize;
-            result.Text = totalMaterialSize.ToString() + sizeType;
+            result.Text = totalMaterialSize.ToString() + " " + sizeType;
 
         }
 
